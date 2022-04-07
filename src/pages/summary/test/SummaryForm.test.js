e99f583296d48199d58c401faefde3b99706fe7b
Varga Dániel <dantschi@gmail.com>
2022-04-07 10:48:27 +0200

    Popover test and component finished

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,9 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import SummaryForm from '../SummaryForm'
 
 describe('tests SummaryForm', () => {
@@ -12,7 +17,7 @@ describe('tests SummaryForm', () => {
     expect(checkbox).not.toBeChecked()
   })
 
-  test('button is disabled till checkbox is not checked', () => {
+  test('button is disabled till checkbox is not checked', async () => {
     render(<SummaryForm />)
     const checkbox = screen.getByRole('checkbox', {
       name: /^I agree to/i,
@@ -21,10 +26,33 @@ describe('tests SummaryForm', () => {
 
     expect(orderButton).toBeDisabled()
 
-    fireEvent.click(checkbox)
+    await userEvent.click(checkbox)
     expect(orderButton).toBeEnabled()
 
-    fireEvent.click(checkbox)
+    await userEvent.click(checkbox)
     expect(orderButton).toBeDisabled()
+  })
+
+  test('popover response to hover', async () => {
+    render(<SummaryForm />)
+    // popover should be hidden on page load
+    const nullPopover = screen.queryByText(
+      /no icecream will actually be delivered/i
+    )
+    expect(nullPopover).not.toBeInTheDocument()
+    // popover appears upon mouseover of checkbox label
+
+    const termsAndConditions = screen.getByText(/terms and conditions/i)
+    userEvent.hover(termsAndConditions)
+
+    // screent.getByText would throw error if it did not find anything, but the best practice is to check it with expect, too
+    const popover = screen.getByText(/no icecream will actually be delivered/i)
+    expect(popover).toBeInTheDocument()
+
+    // popover disappears when we mouse out
+    await userEvent.unhover(termsAndConditions)
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no icecream will actually be delivered/i)
+    )
   })
 })
