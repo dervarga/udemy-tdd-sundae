937b477e4c1716def35a2d88ac16eb4dfb6a6520
Varga Dániel <dantschi@gmail.com>
2022-04-08 11:51:26 +0200

    test for toppings in totalUpdates.test.js

--- src/pages/entry/test/totalUpdates.test.js
+++ src/pages/entry/test/totalUpdates.test.js
@@ -10,7 +10,7 @@ test('updates scoop subtotal when scoops change', async () => {
   expect(scoopsSubtotal).toHaveTextContent('0.00')
 
   // update vanilla scoops to 1 and check the subtotal
-  // await because it will shop up first after server responded
+  // await because it will show up first after server responded
   const vanillaInput = await screen.findByRole('spinbutton', {
     name: 'Vanilla',
   })
@@ -26,4 +26,35 @@ test('updates scoop subtotal when scoops change', async () => {
   userEvent.clear(chocolateInput)
   await userEvent.type(chocolateInput, '2')
   expect(scoopsSubtotal).toHaveTextContent('6.00')
+})
+
+test('update toppings total when topping changes', async () => {
+  render(<Options optionType={OPTION_TYPES.toppings} />)
+
+  // make sure total starts with $0.00
+  const toppingsSubtotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  })
+  expect(toppingsSubtotal).toHaveTextContent('0.00')
+
+  // activate cherries topping and check subtotal
+  // await because it will show up first after server responded
+  const cherriesInput = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  })
+  userEvent.clear(cherriesInput)
+  await userEvent.click(cherriesInput)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+
+  // activate hot fudge topping and check subtotal
+  const hotFudgeInput = await screen.findByRole('checkbox', {
+    name: 'Hot fudge',
+  })
+  userEvent.clear(hotFudgeInput)
+  await userEvent.click(cherriesInput)
+  expect(toppingsSubtotal).toHaveTextContent('3.00')
+
+  // deactivate cherries topping and check subtotal
+  await userEvent.click(cherriesInput)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
 })
